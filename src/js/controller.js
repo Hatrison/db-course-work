d9267e68b96246b71b9f308849f571bcb3441e04
Hatrison <vadimulanovskiy@gmail.com>
2023-01-04 19:17:45 +0200

    Fix bugs with controllers and add screenshots of testing

--- src/js/controller.js
+++ src/js/controller.js
@@ -23,9 +23,7 @@ exports.get = (req, res) => {
 exports.add = (req, res) => {
   const { username, mail, id } = req.body;
   if (!(username && mail && id))
-    return res
-      .status(400)
-      .json({ message: "Username, mail and id are required" });
+    return res.status(400).json("Username, mail and id are required");
   db.query(`SELECT * FROM user WHERE id=${id}`, (err, result) => {
     if (err) return res.status(500).json(err);
     if (result.length !== 0)
@@ -49,31 +47,30 @@ exports.update = (req, res) => {
   const { id } = req.params;
   const { username, mail } = req.body;
   if (!(username || mail))
-    return res.status(400).json({ message: "Required username or mail" });
+    return res.status(400).json("Required username or mail");
   db.query(`SELECT * FROM user WHERE id=${id}`, (err, result) => {
     if (err) return res.status(500).json(err);
     if (result.length === 0)
       return res.status(404).json("No user with this id");
+    if (username) {
+      let query = `UPDATE user SET username='${username}' WHERE id=${id}`;
+      db.query(query, (err) => {
+        if (err) return res.status(500).json(err);
+      });
+    }
+    if (mail) {
+      let query = `UPDATE user SET mail='${mail}' WHERE id=${id}`;
+      db.query(query, (err) => {
+        if (err) return res.status(500).json(err);
+      });
+    }
+    res.status(200).json(`User with id ${id} was updated`);
   });
-  let query = "";
-  if (username) {
-    query = `UPDATE user SET username='${username}' WHERE id=${id}`;
-    db.query(query, (err) => {
-      if (err) return res.status(500).json(err);
-    });
-  }
-  if (mail) {
-    query = `UPDATE user SET username='${mail}' WHERE id=${id}`;
-    db.query(query, (err) => {
-      if (err) return res.status(500).json(err);
-    });
-  }
-  res.status(200).json({ message: `User with id ${id} was updated` });
 };
 
 exports.delete = (req, res) => {
   const { id } = req.params;
-  if (!id) return res.status(400).json({ message: "Id required" });
+  if (!id) return res.status(400).json("Id required");
   const query = `DELETE FROM user WHERE id=${id}`;
   db.query(`SELECT * FROM user WHERE id=${id}`, (err, result) => {
     if (err) return res.status(500).json(err);
